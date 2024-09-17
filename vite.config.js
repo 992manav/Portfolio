c6adc6d2ec1a0030f0ef1147dc7b227eb5a23910
992manav <dmanavpianist@gmail.com>
2024-09-18 03:14:10 +0530

    image

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  publicDir: "public", // Ensure Vite serves static files from this directory
+  plugins: [react()], // Ensure Vite serves static files from this directory
 });
